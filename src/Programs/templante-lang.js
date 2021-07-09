f9feeff9f0d2444a188ffc4e47f5c6b70ac6f8b6
diegodisant <developerdiego0@gmail.com>
2021-07-08 21:06:14 -0500

    challenge: create a template language

---
+++ src/Programs/templante-lang.js
@@ -0,0 +1,18 @@
+/**
+ * DESCRIPTION
+ * Engenieering team needs create a new template language it MUST have following requirements:
+ * 1. Variables.
+ * 2. Operations (lower, upper, trim, reverse)
+ *
+ * Pre processed template:
+ * <my-component>
+ *   <image source="{{ %component.imageSource% }}"></image>
+ *   <text-card>{{ %component.description% | reverse (can be: lower, upper, trim, reverse)}}</text-card>
+ * </my-component>
+ *
+ * Processed template:
+ * <my-component>
+ *   <image source="https://my-cdn.site.com/cat-devil.jpeg"></image>
+ *   <text-card>noitpircsed a si siht (original text: this is a description)</text-card>
+ * </my-component>
+ */
